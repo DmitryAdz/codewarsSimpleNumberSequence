90de39d5e923f6fad5b0ea7c5ae231cdf9618d49
odinets <odinets@irc.beltelecom.by>
2020-10-21 09:30:44 +0300

    1

--- codewarsSimpleNumberSequence/src/by/odinets/codewars/simpleNumberSequence/RunnerSimpleNumb.java
+++ codewarsSimpleNumberSequence/src/by/odinets/codewars/simpleNumberSequence/RunnerSimpleNumb.java
@@ -4,10 +4,42 @@ public class RunnerSimpleNumb {
 
 	public static void main(String[] args) {
 	
-		String rowStr = "1346";
-		int result = Solution.missing(rowStr);
-		System.out.println("rowStr :: " + rowStr);
-		System.out.println("result solution :: " + result);
+		String[] rowStr = {"123567",
+							//"899091939495",
+							//"9899101102",
+							//"599600601602",
+							//"8990919395",
+							//"998999100010011003",
+							//"99991000110002",
+							//"979899100101102",
+							//"9394969798",
+							"92939495969798100101102103",
+							"900001900002900004900005900006"};
+		/*
+		 * assertEquals(4,Solution.missing("123567"));
+        assertEquals(92,Solution.missing("899091939495"));
+        assertEquals(100,Solution.missing("9899101102"));
+        assertEquals(-1,Solution.missing("599600601602"));
+        assertEquals(-1,Solution.missing("8990919395"));
+        assertEquals(1002,Solution.missing("998999100010011003"));
+        assertEquals(10000,Solution.missing("99991000110002"));
+        assertEquals(-1,Solution.missing("979899100101102"));
+        assertEquals(900003,Solution.missing("900001900002900004900005900006"));
+		 */
+		for(String el : rowStr) {
+			System.out.println("********************************************");
+			System.out.println("rowStr :: " + el);
+			int result = Solution.missing(el);
+			System.out.println("result solution :: " + result);
+		}
+		
+		/*for(String el : rowStr) {
+			System.out.println("********************************************");
+			int result = Solution1.missing(el);
+			System.out.println("rowStr :: " + el);
+			System.out.println("result solution :: " + result);
+		}*/
+		
 	}
 	
 }
--- codewarsSimpleNumberSequence/src/by/odinets/codewars/simpleNumberSequence/Solution1.java
+++ codewarsSimpleNumberSequence/src/by/odinets/codewars/simpleNumberSequence/Solution1.java
@@ -1,46 +1,25 @@
-
 package by.odinets.codewars.simpleNumberSequence;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/*
-* In this Kata, you will be given a string of numbers in sequence and your task will be to return the missing number. 
-* If there is no number missing or there is an error in the sequence, return -1.
-* For example:
-*
-*missing("123567") = 4 
-*missing("899091939495") = 92
-*missing("9899101102") = 100
-*missing("599600601602") = -1 -- no number missing
-*missing("8990919395") = -1 -- error in sequence. Both 92 and 94 missing.
-*
-*The sequence will always be in ascending order.
-*
-*More examples in the test cases.
-*
-*Good luck!
-*/
-
-public class Solution {
+public class Solution1 {
 
 	public static int numberSearch = -1;
 	
 	public static int missing(String s){
 	
 		int lengthStr = s.length();
-		boolean isSequence = false;	//флаг последовательности чисел
+		boolean isSequence = false;
 		for(int i = 1; i <= lengthStr; i++) {
 			//System.out.println("********************************************");
 			if(!isSequence) {
 				//System.out.println("i :: " + i);
-				boolean isCutStep = (lengthStr >= i*2);		//флаг шага обрезки строки чисел
-				if(isCutStep) {
-					Integer[] numbers = cutString(s, i);	//режем строку на куски
-					if(numbers == null) {
-						continue;
-					}
-					isSequence = checkIsSequence(numbers);	//проверяем составляют ли куски последовательность чисел
+				boolean isSequenceStep = (lengthStr >= i*2);
+				if(isSequenceStep) {
+					String[] strNumb = cutString(s, i);
+					System.out.println("strNumb :: " + Arrays.toString(strNumb));
+					isSequence = checkIsSequence(strNumb);
 				}
 			} else {
 				break;
@@ -56,49 +35,25 @@ public class Solution {
 	 * @param numbDigits
 	 * @return
 	 */
-	public static Integer[] cutString(String str, int numbDigits) {
-		ArrayList<Integer> arrNumbers = new ArrayList<Integer>();
+	public static String[] cutString(String str, int numbDigits) {
+		ArrayList<String> arrNumbers = new ArrayList<String>();
 		int indexLetter = 0;
-		boolean flagIsNextRank = false;			//флаг повышения разряда, надо резать на +1 символ
-		boolean flagIsNextBeforeRank = false;	//флаг предповышения разраяда, надо проверить есть ли следующий элемент
-		Integer[] result = null;
+		boolean flagIsNextRank = false;
 		while(indexLetter + numbDigits <= str.length()) {
-			//System.out.println("numbDigits :: " + numbDigits);
 			String number = str.substring(indexLetter, indexLetter + numbDigits);
-			System.out.println("number :: " + number);
-			try {
-				arrNumbers.add(Integer.parseInt(number)); 
-			} catch(Exception ex) {
-				System.out.println("ex :: " + ex);
-				return result;
-			}
+			arrNumbers.add(number);
 			indexLetter = indexLetter + numbDigits;
-			
 			if(!flagIsNextRank) {
 				if(isNextRank(number)) {
 					numbDigits++;
 					flagIsNextRank = true;
 				}
 			}
-			
-			if(!flagIsNextBeforeRank) {
-				//проверяем является ли число 98 или 998 или 9998
-				if(isNextBeforeRank(number)) {
-					flagIsNextBeforeRank = true;
-				}
-			} else {
-				//проверяем следующее число или 99 или 100
-				if(isNextRank(number)) {
-					numbDigits++;
-					flagIsNextRank = true;
-				} else {
-					
-				}
-			}
 		}
-		//System.out.println("arrNumbers :: " + arrNumbers);
+		
+		String[] result = null;
 		if(!arrNumbers.isEmpty()) {
-			result = new Integer[arrNumbers.size()];
+			result = new String[arrNumbers.size()];
 			for(int i = 0; i < arrNumbers.size(); i++) {
 				result[i] = arrNumbers.get(i);
 			}
@@ -128,7 +83,7 @@ public class Solution {
 	 * @param numbers
 	 * @return
 	 */
-	public static boolean checkIsSequence(Integer[] numbers) {
+	public static boolean checkIsSequence(String[] numbers) {
 		boolean isSequence = false;	
 		boolean flagMissingNumber = false;
 		numberSearch = -1;
@@ -166,13 +121,64 @@ public class Solution {
 	 * @param numb2
 	 * @return 1 - числа идут последовательно, numb1+1 - между числами пропущено число numb1+1,  исключение предыдущих вариантов
 	 */
-	public static int searchNumbMissing(int numb1, int numb2) {
-		if(numb2 - numb1 == 1) {
+	public static int searchNumbMissing(String numb1, String numb2) {
+		int lengthN1 = numb1.length();
+		int lengthN2 = numb2.length();
+		String lastNumb1 = numb1.substring(lengthN1 - 1, lengthN1);
+		String lastNumb2;
+		if(isNextRank(numb1)) {
+			lastNumb1 = numb1;
+			if(lengthN2 == 1) {
+				return 0;
+			}
+			lastNumb2 = numb2;
+		} else {
+			if(isNextRank(lastNumb1)) {
+				if(lengthN1 > 1) {
+					lastNumb1 = numb1.substring(lengthN1 - 2, lengthN1);
+				}
+				if(lengthN2 == 1) {
+					return 0;
+				}
+				lastNumb2 = numb2.substring(lengthN2 - 2, lengthN2);
+			} else {
+				lastNumb2 = numb2.substring(lengthN2 - 1, lengthN2);
+			}
+			
+		}
+			
+		System.out.println("lastNumb1 :: " + lastNumb1);
+		System.out.println("lastNumb2 :: " + lastNumb2);
+		
+		if(isNextRank(lastNumb1)) {
+			String strEquals = "1";
+			String strNextEquals = "1";
+			for(int i = 1; i <= lastNumb1.length(); i++) {
+				strEquals += "0";
+				if(i < lastNumb1.length()) {
+					strNextEquals += "0";
+				} else {
+					strNextEquals += "1";
+				}
+				
+			}
+			if(lastNumb2.equals(strEquals)) {
+				System.out.println("sequence :: " + lastNumb1 + " , " + lastNumb2);
+				return 1;
+			}
+			if(lastNumb2.equals(strNextEquals)) {
+				System.out.println("sequence :: " + lastNumb1 + " , " + lastNumb2);
+				return 1;
+			}
+		} else {
+			
+		}
+		/*if(numb2 - numb1 == 1) {
 			return 1;
 		}
 		if(numb2 - numb1 == 2) {
 			return ++numb1;
-		}
+		}*/
 		return 0;
 	}
 }
